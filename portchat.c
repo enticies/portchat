5c5e6e0a35acb036d44dba0a0e2d7864a72c2f27
enticies <62285913+enticies@users.noreply.github.com>
2021-07-13 09:47:24 -0400

    Add files via upload
    "printerror" header file has been moved to the main file for clarity.
    
    Bug in the outputs where the program would print empty response messages has been fixed.
    
    Program now takes the number of columns from environment variables and prints the coming responses at a fixed distance from left. Though, this causes a problem when the terminal size is changed, where the the program prints the messages at the same distance from the left. Additionally, if the coming message is too long the rest of the message is printed from the beginning from left, for UI purposes, this should be fixed.
    
    If a large text is copy/pasted, program prints the text until a newline is found. The user has to enter a newline again to send the rest. 
    
    Program also needs to handle EOF and close the program. This feature should be easy to implement.

--- portchat.c
+++ portchat.c
@@ -7,21 +7,29 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include "printerror.h"
+#include <sys/ioctl.h>
 
 #define ANSI_COLOR_GREEN   "\e[0;32m"
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 void menu();
-void createServer();
-void conServer(char *ip, int);
-void * sendInput(void * arg);
+void createServer(int, int);
+void conServer(char *ip, int, int);
+void * getInput(void * arg);
 void usage();
+void coloring();
 int checkPort(char *);
 
 char input[99999];
 int sendFlag = 0;
 
+int input_length;
+
 int main(int argc, char **argv) {
+  struct winsize w;
+  ioctl(0, TIOCGWINSZ, &w);
+  
   if (argc < 2) {
     printError("usage");
   }
@@ -32,11 +40,11 @@ int main(int argc, char **argv) {
     }
     else if ((strcmp(argv[1], "-c")) == 0 && argc == 4) {
       int port = checkPort(argv[3]);
-      conServer(argv[2], port);
+      conServer(argv[2], port, w.ws_col);
     }
     else if ((strcmp(argv[1], "-s")) == 0 && argc == 3) {
       int port = checkPort(argv[2]);
-      createServer(port);
+      createServer(port, w.ws_col);
     }
     else {
       usage();
@@ -78,15 +86,15 @@ int checkPort(char * port) {
   exit(1);
 }
 
-void createServer(int port) {
-  char server_message[] = "You have connected to the server!\n";
-
+void createServer(int port, int columns) {
+  char server_message[] = "You have connected to the server!\n\n";
   pthread_t thread1;
-  pthread_create(&thread1, NULL, sendInput, NULL);
+  pthread_create(&thread1, NULL, getInput, NULL);
 
   // create the server socket
-  int server_socket;
+  long server_socket;
   server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
 
   // define the server address
   struct sockaddr_in server_address;
@@ -97,10 +105,11 @@ void createServer(int port) {
   int server_status = bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
 
   if (server_status == 0) {
-    printf(ANSI_COLOR_GREEN "\nServer has been successfully created!\n" ANSI_COLOR_RESET "\n");
+    printf("Server has been successfully created!\t\t\tPort: %d\n\n", port);
   }
   else {
     printError("server creation");
+    exit(1);
   }
 
   listen(server_socket, 1);
@@ -109,40 +118,45 @@ void createServer(int port) {
   client_socket = accept(server_socket, NULL, NULL);
 
   if (client_socket > -1) {
-    printf(ANSI_COLOR_GREEN "\nSomeone has connected to the server!\n" ANSI_COLOR_RESET "\n");
+    printf("Someone has connected to the server!\n\n");
+    pthread_create(&thread1, NULL, getInput, NULL); // don't need to create every loop
     send(client_socket, server_message, sizeof(server_message), MSG_DONTWAIT);
-
   }
 
   while (client_socket > -1) {
     char client_response[99999] = "\n";
+
     int length = recv(client_socket, &client_response, sizeof(client_response), MSG_DONTWAIT);
 
     if (length == 0) {
       printError("closed connection");
+      exit(0);
       break;
     }
+
     if (sendFlag == 1) {
-      send(client_socket, input, sizeof(input), 0);
+      send(client_socket, input, strlen(input), 0);
       sendFlag = 0;
+      pthread_create(&thread1, NULL, getInput, NULL);
     }
-    /* client_response[length] = '\0'; */
-    if (strcmp(client_response, "\n") != 0) {
-      printf("%*c%s", 50, ' ', client_response);
+    client_response[length] = '\0';
+    if (length > 0 && strcmp(client_response, "\n") != 0) {
+      printf("%*sOther Side: %s\n", (int) ((float) columns * 0.5),  " ", client_response);
     }
-    pthread_create(&thread1, NULL, sendInput, NULL);
   }
 
   // close the socket
   close(server_socket);
+  // end ncurses
+  printError("closed connection");
 }
 
-void conServer(char * ip, int port) {
+void conServer(char * ip, int port, int columns) {
   // create a socket
-  int network_socket;
+  long network_socket;
 
   pthread_t thread1;
-  pthread_create(&thread1, NULL, sendInput, NULL);
+  pthread_create(&thread1, NULL, getInput, NULL);
 
   network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -151,6 +165,7 @@ void conServer(char * ip, int port) {
   server_address.sin_family = AF_INET;
   server_address.sin_port = htons(port);
   server_address.sin_addr.s_addr = INADDR_ANY;
+
   if (inet_pton(AF_INET, ip, &(server_address.sin_addr)) < 1) {
     printError("ip");
     exit(1);
@@ -163,9 +178,10 @@ void conServer(char * ip, int port) {
     return;
   }
   else {
-    printf(ANSI_COLOR_GREEN "\nConnection to the server established.\n" ANSI_COLOR_RESET "\n");
+    printf("Connection to the server established.\n");
  }
 
+  pthread_create(&thread1, NULL, getInput, NULL);
   while (connection_status == 0) {
     char server_response[99999] = "\n";
     int length = recv(network_socket, &server_response, sizeof(server_response), MSG_DONTWAIT);
@@ -174,20 +190,23 @@ void conServer(char * ip, int port) {
       printError("closed connection");
       return;
     }
-    if (sendFlag == 1) {
-      send(network_socket, input, sizeof(input), 0);
+
+    if (sendFlag == 1 && strcmp(input, "\n") != 0) {
+      send(network_socket, input, strlen(input), 0);
       sendFlag = 0;
+      pthread_create(&thread1, NULL, getInput, NULL);
     }
-    /* server_response[length] = '\0'; */
-    if (strcmp(server_response, "\n") != 0) {
-      printf("%*c%s", 50, ' ', server_response);
+    server_response[length] = '\0';
+    if (length > 0 && strcmp(server_response, "\n") != 0) {
+      printf("%*sOther Side: %s\n", (int) ((float) columns * 0.5),  " ", server_response);
   }
-    pthread_create(&thread1, NULL, sendInput, NULL);
   }
+  printError("closed connection");
 }
 
-void* sendInput(void * arg) {
+void* getInput(void * arg) {
   if (strlen(fgets(input, sizeof(input), stdin)) < 100000) {
+    input[strlen(input)-1] = '\0';
     sendFlag = 1;
   }
   else {
@@ -195,4 +214,34 @@ void* sendInput(void * arg) {
   }
   return NULL;
 }
-
+void printError(char * errorType) {
+  // input related
+  if (strcmp(errorType, "large input") == 0) {
+    printf(ANSI_COLOR_RED "Input too large.\n" ANSI_COLOR_RESET "\n");
+  }
+  else if (strcmp(errorType, "usage") == 0) {
+    printf("Incorrect usage. Use the -h flag to see the correct usage.\n");
+  }
+  else if (strcmp(errorType, "incorrect input") == 0) {
+    printf(ANSI_COLOR_RED "\nIncorrect input.\n" ANSI_COLOR_RESET "\n");
+  }
+  else if (strcmp(errorType, "incorrect port") == 0) {
+    printf(ANSI_COLOR_RED "\nIncorrect port number.\n" ANSI_COLOR_RESET "\n");
+  }
+  else if (strcmp(errorType, "range error") == 0) {
+    printf(ANSI_COLOR_RED "\nNumber must be in range 0-63335.\n" ANSI_COLOR_RESET "\n");
+  }
+  // connection related
+  else if (strcmp(errorType, "closed connection") == 0) {
+    printf(ANSI_COLOR_RED "\nConnection closed.\n" ANSI_COLOR_RESET "\n");
+  }
+  else if (strcmp(errorType, "server creation") == 0) {
+    printf(ANSI_COLOR_RED "\nUnable to create the server.\n" ANSI_COLOR_RESET "\n");
+  }
+  else if (strcmp(errorType, "not connected") == 0) {
+    printf(ANSI_COLOR_RED "\nCouldn't connect to the server.\n" ANSI_COLOR_RESET "\n");
+  }
+  else if (strcmp(errorType, "ip") == 0) {
+    printf(ANSI_COLOR_RED "\nInvalid IP address.\n" ANSI_COLOR_RESET "\n");
+  }
+}
